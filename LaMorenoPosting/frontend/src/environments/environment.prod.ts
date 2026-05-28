7e256108e5ca239364e8d129aed2fe82bb3fcf62
tobias perea <tobiasperea>
2026-05-27 21:04:16 -0300

    cambio el deploy a render

--- LaMorenoPosting/frontend/src/environments/environment.prod.ts
+++ LaMorenoPosting/frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://tobias-perea-tp2-prog4-2026-c1-production.up.railway.app'
+  apiUrl: 'https://tobias-perea-tp2-prog4-2026-c1.onrender.com'
 };
