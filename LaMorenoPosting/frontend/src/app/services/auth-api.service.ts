e8b6c0529ef2167fe9165e1282a72257de4fc4aa
tobias perea <tobiasperea>
2026-06-18 16:43:20 -0300

    fix epico

--- LaMorenoPosting/frontend/src/app/services/auth-api.service.ts
+++ LaMorenoPosting/frontend/src/app/services/auth-api.service.ts
@@ -1,43 +1,29 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthApiService {
 
-  private apiUrl = 'http://localhost:3000/auth';
+  private apiUrl = `${environment.apiUrl}/auth`;
 
-  constructor(
-    private http: HttpClient
-  ) { }
+  constructor(private http: HttpClient) {}
 
   autorizar(token: string) {
-
     return this.http.post(
       `${this.apiUrl}/autorizar`,
       {},
-      {
-        headers: {
-          Authorization: `Bearer ${token}`
-        }
-      }
+      { headers: { Authorization: `Bearer ${token}` } }
     );
-
   }
 
   refrescar(token: string) {
-
     return this.http.post(
       `${this.apiUrl}/refrescar`,
       {},
-      {
-        headers: {
-          Authorization: `Bearer ${token}`
-        }
-      }
+      { headers: { Authorization: `Bearer ${token}` } }
     );
-
   }
-
 }
